72a3b638e0886d55dd433cd6bca163f8d3d1b368
rbowen <rbowen@rcbowen.com>
2020-07-07 15:43:30 -0400

    Count Reddit members.

---
+++ reddit.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+# How many followers do you have?
+import urllib.request
+import re
+
+subreds = [
+        'CentOS','redhat','gluster','ovirt','ceph','Fedora','openshift','ansible'
+        ];
+for subred in subreds:
+    req = urllib.request.Request( 'https://reddit.com/r/' + subred,
+        data = None,
+        headers={
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        }   )
+    f = urllib.request.urlopen(req)
+    html = f.read().decode('utf-8')
+    # print (html)
+
+#  Looks like ...
+#           <div class="_3XFx6CfPlg-4Usgxm0gK8R">13.3k</div><p class="_29_mu5qI8E1fq6Uq5koje8" id="IdCard--Subscribers--undefined--t5_2s6fk">Members</p>
+
+    print ( subred + ': ' + re.search('.*?([\d\.k]+?)</div><p class=.*?id="IdCard\-\-Subscribers\-\-.*', html).group(1) )
+
